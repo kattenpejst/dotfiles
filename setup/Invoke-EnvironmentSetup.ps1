14d0e8539a1f0b0857cb7087da03d355b57a4815
pkorsback <peter.korsback@creuna.se>
2017-11-07 09:25:49 +0100

    Add setup of VS Code settings file

--- setup/Invoke-EnvironmentSetup.ps1
+++ setup/Invoke-EnvironmentSetup.ps1
@@ -1,1 +1,2 @@
 cmd /c mklink /h "c:\Program Files (x86)\cmder\vendor\conemu-maximus5\ConEmu.xml" "C:\Users\pkorsback\dotfiles\ConEmu.xml"
+cmd /c mklink /h "c:\Users\pkorsback\AppData\Roaming\Code\User\settings.json" "C:\Users\pkorsback\dotfiles\settings.json"
