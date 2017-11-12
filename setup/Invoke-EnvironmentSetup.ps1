210251c1d40652546bc9ba0d83e0808f671ec162
Peter Korsbäck <peter.korsback@gmail.com>
2017-11-12 09:29:42 +0100

    Change user name in setup file
    Of course, this should be an argument to the script but
    who has the energy...

--- setup/Invoke-EnvironmentSetup.ps1
+++ setup/Invoke-EnvironmentSetup.ps1
@@ -1,2 +1,2 @@
-cmd /c mklink /h "c:\Program Files (x86)\cmder\vendor\conemu-maximus5\ConEmu.xml" "C:\Users\pkorsback\dotfiles\ConEmu.xml"
-cmd /c mklink /h "c:\Users\pkorsback\AppData\Roaming\Code\User\settings.json" "C:\Users\pkorsback\dotfiles\settings.json"
+cmd /c mklink /h "c:\Program Files (x86)\cmder\vendor\conemu-maximus5\ConEmu.xml" "C:\Users\korsa\dotfiles\ConEmu.xml"
+cmd /c mklink /h "c:\Users\korsa\AppData\Roaming\Code\User\settings.json" "C:\Users\korsa\dotfiles\settings.json"
