5a2c1f7e74a4b93440457fbd5557d370daedfce1
Peter Korsbäck <peter.korsback@gmail.com>
2018-05-03 07:03:06 +0200

    Fix path for cmder when installed using chocolatey

--- setup/Invoke-EnvironmentSetup.ps1
+++ setup/Invoke-EnvironmentSetup.ps1
@@ -1,5 +1,5 @@
 # TODO: add user name parameter
 
-cmd /c mklink /h "C:\Program Files (x86)\cmder\vendor\conemu-maximus5\ConEmu.xml" "C:\Users\korsa\dotfiles\ConEmu.xml"
+cmd /c mklink /h "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml" "C:\Users\korsa\dotfiles\ConEmu.xml"
 cmd /c mklink /h "C:\Users\korsa\AppData\Roaming\Code\User\settings.json" "C:\Users\korsa\dotfiles\settings.json"
 cmd /c mklink /h "C:\Users\korsa\AppData\Roaming\Keypirinha\User\Keypirinha.ini" "C:\Users\korsa\dotfiles\Keypirinha.ini"
