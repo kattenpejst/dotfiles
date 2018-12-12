e39985dc22792cac0e731d9b7bda6283cd31eaeb
Peter Korsbäck <peter.korsback@qliro.com>
2018-12-12 16:16:31 +0100

    Add .bash_profile and setup

--- setup/Invoke-EnvironmentSetup.ps1
+++ setup/Invoke-EnvironmentSetup.ps1
@@ -4,3 +4,4 @@ cmd /c mklink /h "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml" "C:\Users\ko
 cmd /c mklink /h "C:\Users\korsa\AppData\Roaming\Code\User\settings.json" "C:\Users\korsa\dotfiles\settings.json"
 cmd /c mklink /h "C:\Users\korsa\AppData\Roaming\Keypirinha\User\Keypirinha.ini" "C:\Users\korsa\dotfiles\Keypirinha.ini"
 cmd /c mklink /h "C:\Users\korsa\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" "C:\Users\korsa\dotfiles\Microsoft.PowerShell_profile.ps1"
+cmd /c mklink /h "C:\Users\korsa\.bash_profile" "C:\Users\korsa\dotfiles\.bash_profile
