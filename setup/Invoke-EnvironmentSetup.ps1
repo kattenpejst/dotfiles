10d8c1a71a7e97cf3f79c73c0e7009106b5f1854
Peter Korsbäck <peter.korsback@gmail.com>
2020-12-05 19:41:57 +0100

    Remove cmder settings

--- setup/Invoke-EnvironmentSetup.ps1
+++ setup/Invoke-EnvironmentSetup.ps1
@@ -6,7 +6,6 @@ param(
     $username
 )
 
-cmd /c mklink /h "C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml" "C:\Users\$username\dotfiles\ConEmu.xml"
 cmd /c mklink /h "C:\Users\$username\AppData\Roaming\Code\User\settings.json" "C:\Users\$username\dotfiles\settings.json"
 cmd /c mklink /h "C:\Users\$username\AppData\Roaming\Keypirinha\User\Keypirinha.ini" "C:\Users\$username\dotfiles\Keypirinha.ini"
 cmd /c mklink /h "C:\Users\$username\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" "C:\Users\$username\dotfiles\Microsoft.PowerShell_profile.ps1"
