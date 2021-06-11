16417cc2deffd0e8bd9a576be561ac9cccac3241
Peter Korsbäck <peter.korsback@gmail.com>
2021-06-11 08:27:14 +0200

    Fix install script for Windows Terminal

--- setup/Invoke-EnvironmentSetup.ps1
+++ setup/Invoke-EnvironmentSetup.ps1
@@ -10,4 +10,4 @@ cmd /c mklink /h "C:\Users\$username\AppData\Roaming\Code\User\settings.json" "C
 cmd /c mklink /h "C:\Users\$username\AppData\Roaming\Keypirinha\User\Keypirinha.ini" "C:\Users\$username\dotfiles\Keypirinha.ini"
 cmd /c mklink /h "C:\Users\$username\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" "C:\Users\$username\dotfiles\Microsoft.PowerShell_profile.ps1"
 cmd /c mklink /h "C:\Users\$username\.bash_profile" "C:\Users\$username\dotfiles\.bash_profile"
-cmd /c mklink /h "C:\Users\$username\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" "C:\Users\$username\dotfiles\windows-terminal-settings.json"
+cmd /c mklink /h "C:\Users\$username\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" "C:\Users\$username\dotfiles\windows-terminal\settings.json"
